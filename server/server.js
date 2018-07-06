de02df8f225bd80a0d3ca0e6ff19c8fe4c11e8f7
ruinan.li02 <ruinan.li02@ele.me>
2018-07-06 16:10:15 +0800

    feat:增加express+mongodb基础代码

--- server/server.js
+++ server/server.js
@@ -1,45 +1,53 @@
 const express = require('express')
-const mongoose =  require('mongoose')
-// 连接mongodb 并且使用imooc这个集合
-const DB_URL = 'mongodb://localhost:27017/imooc'
+const mongoose = require('mongoose')
+// 链接mongo 并且使用imooc这个集合，没有回自动帮忙建
+const DB_URL = 'mongodb://127.0.0.1:27017/imooc'
 mongoose.connect(DB_URL)
 mongoose.connection.on('connected', function() {
-  console.log('mongo connect success')
+  console.log('mongo connect success!')
 })
 
-// 类似mysql的表，mongo里有文档、字段的概念
+// 类似mysql的表，mongo里有文档的概念 文档模型《----》表
+// 说明文档叫user，也就是表叫user，表中的值，也就是说文档内容呢，通过new mongoose.Schema({})定义
 const User = mongoose.model('user', new mongoose.Schema({
   user: { type: String, require: true },
   age: { type: Number, require: true }
 }))
-// 新增数据
-User.create({
-  user: 'xiaohua',
-  age: 22
-}, function(err, doc) {
-  if (!err) {
-    console.log(doc)
-  } else {
-    console.log(err)
-  }
-})
-// User.remove({age: 18}, function(err, doc) {
+
+// mongoose 对数据的操作，第一个参数是 过滤的对象 相当于mysql中的where
+// create 新增数据
+// User.create({
+//   user: 'imoocer',
+//   age: 23
+// }, function(err, doc) {
+//   if (!err) {
+//     console.log(doc) // 就是数据
+//   } else {
+//     console.log(err)
+//   }
+// })
+
+// 删除数据
+// User.remove({age: 23}, function(err, doc) {
 //   console.log(doc)
 // })
-User.update({'user': 'xiaoming'}, {'$set': {age: 26}}, function(err, doc) {
+
+// 更新
+User.update({'user': 'imoocer'}, {'$set': {age: 33}}, function(err, doc) {
   console.log(doc)
 })
+
 const app = express()
 
 app.get('/', function(req, res) {
-  res.send('<h1>hello world!</h1>')
+  res.send('<h1>hello world</h1>')
 })
 
 app.get('/data', function(req, res) {
-  User.findOne({user: 'xiaoming'}, function(err, doc) {
-    res.json(doc)
+  User.find({}, function(err, doc) {
+    res.json(doc) // doc就是数据
   })
-  // res.json({name: 'imooc React app', type: 'IT'})
+  // res.json({name: 'imooc React', type: 'IT'})
 })
 
 app.listen(9093, function() {
